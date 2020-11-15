dc8a25cea9fc036f7ad9af6fb799b76640786f3d
YakovSpb <yakov_dux@bk.ru>
2020-11-15 14:54:07 +0300

    Start

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Header = () => {
+	return (
+    <header className = "header">
+    	<div class="header__title">
+    	Поселок в подмосковье
+    	<span>социальная сеть</span>
+    	</div>
+       <img src='https://vcube.pro/images/Construction_services/Sections/kottedzhnyy-poselok.png' />
+    </header>
+		)
+}
+
+export default  Header;
