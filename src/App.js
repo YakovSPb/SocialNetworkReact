f36b1400dd924a5ebaf7ec5262aaaba5e4ab898a
YakovSpb <yakov_dux@bk.ru>
2020-12-12 09:09:20 +0300

    60

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ const App = (props) => {
       <Sidebar />
       <div className ='app-wrapper-content'>
       	<Route exact path='/dialogs' render={ () => <DialogsContainer /> } />
-        <Route path='/profile' render={ () => <ProfileContainer /> } />
+        <Route path='/profile/:userId?' render={ () => <ProfileContainer /> } />
         <Route path='/users' render={ () => <UsersContainer /> } />
       	<Route path='/news' component={News}/>
       	<Route path='/music' component={Music}/>
