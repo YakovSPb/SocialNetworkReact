4a9378be898b7ad7105a0a1c8d9a675380370749
YakovSPb <yakov_dux@bk.ru>
2021-02-18 17:58:36 +0300

    98

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -8,12 +8,15 @@ import {Redirect} from 'react-router-dom';
 import style from "./../common/FormsControls/FormsControls.module.css";
 
 
-const LoginForm = ({handleSubmit, error}) => {
+const LoginForm = ({handleSubmit, error, captchaUrl}) => {
     return (
         <form onSubmit={handleSubmit}>
             {createField("Email", "email", [required], Input)}
             {createField("Password", "password", [required], Input, {type: "password"})}
             {createField(null, "rememberMe", [], Input, {type: "checkbox"}, "remember me")}
+
+            {captchaUrl && <img src={captchaUrl} />}
+            {captchaUrl &&  createField("Symbols from image", "captcha", [required], Input, {})}
 
             {error && <div className={style.formSummaryError}>
                 {error}
@@ -29,19 +32,20 @@ const LoginForm = ({handleSubmit, error}) => {
 const LoginReduxForm = reduxForm({form: 'login'})(LoginForm)
 const Login = (props) => {
     const onSubmit = (formData) => {
-        props.login(formData.email, formData.password, formData.rememberMe);
+        props.login(formData.email, formData.password, formData.rememberMe, formData.captcha);
     }
 
     if (props.isAuth) {
         return <Redirect to={"/profile"}/>
     }
 
-    return <div>
+    return <div>captchaUrl
         <h1>Login</h1>
-        <LoginReduxForm onSubmit={onSubmit}/>
+        <LoginReduxForm onSubmit={onSubmit} captchaUrl={props.captchaUrl}/>
     </div>
 }
 const mapStateToProps = (state) => ({
+    captchaUrl: state.auth.captchaUrl,
     isAuth: state.auth.isAuth
 })
 export default connect(mapStateToProps, {login})(Login);
--- src/redux/auth-reducer.js
+++ src/redux/auth-reducer.js
@@ -1,24 +1,26 @@
-import {authAPI} from '../api/api';
+import {authAPI, securityAPI} from '../api/api';
 import {stopSubmit} from 'redux-form';
 
 const SET_USER_DATA = 'samurai-network/auth/SET_USER_DATA';
+const GET_CAPTCHA_URL_SUCCESS = 'samurai-network/auth/GET_CAPTCHA_URL_SUCCESS';
 
 
 let initialState = {
     userId: null,
     login: null,
     email: null,
-    isAuth: false
+    isAuth: false,
+    captchaUrl: null
 };
 
 const authReducer = (state = initialState, action) => {
     switch (action.type) {
         case SET_USER_DATA:
+        case GET_CAPTCHA_URL_SUCCESS:
             return {
                 ...state,
                 ...action.payload
             }
-
         default:
             return state;
     }
@@ -27,6 +29,10 @@ const authReducer = (state = initialState, action) => {
 export const setAuthUserData = (userId, email, login, isAuth) => ({
     type: SET_USER_DATA, payload:
         {userId, email, login, isAuth}
+})
+
+export const getCaptchaUrlSuccess = (captchaUrl) => ({
+    type: GET_CAPTCHA_URL_SUCCESS, payload: {captchaUrl}
 })
 
 export const getAuthUserData = () => async (dispatch) => {
@@ -38,15 +44,26 @@ export const getAuthUserData = () => async (dispatch) => {
     }
 }
 
-export const login = (email, password, rememberMe) => async (dispatch) => {
-    let response = await authAPI.login(email, password, rememberMe);
+export const login = (email, password, rememberMe, captcha) => async (dispatch) => {
+    let response = await authAPI.login(email, password, rememberMe, captcha);
     if (response.data.resultCode === 0) {
         dispatch(getAuthUserData())
     } else {
+        if (response.data.resultCode === 10) {
+            dispatch(getCaptchaUrl());
+        }
         let message = response.data.messages.length > 0 ? response.data.messages[0] : "Some error";
         dispatch(stopSubmit("login", {_error: message}));
     }
 }
+
+
+export const getCaptchaUrl = () => async (dispatch) => {
+    const response = await securityAPI.getCaptchaUrl();
+    const captchaURl = response.data.url;
+    dispatch(getCaptchaUrlSuccess(captchaURl));
+}
+
 
 export const logout = () => async (dispatch) => {
     let response = await authAPI.logout();
